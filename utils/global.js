419c8e899b469c017df9a9d59fd22db642a02d5b
mayks1 <mayks1@gmail.com>
2022-06-08 20:18:35 +0300

    Change Main Logo

--- utils/global.js
+++ utils/global.js
@@ -4,16 +4,18 @@
   access any variable via object syntax global.yourVarName
 */
 export default {
-  siteUrl: "https://mspase.com",
-  siteName: "MSpase Блог",
-  author: "Mayks",
-  twitterHandle: "@mayks",
-  twitterURL: "",
-  githubURL: "",
-  siteTitle: "Блог за авторски материали, интересни новини и истории",
+  siteUrl: 'https://mspase.com',
+  siteName: 'MSpase Блог',
+  author: 'Mayks',
+  twitterHandle: '@mayks',
+  twitterURL: '',
+  githubURL: '',
+  siteTitle: 'Блог за авторски материали, интересни новини и истории',
   siteDesc:
-    "MSpase.com - ви предоставя интересни новини и истории от България и света, свободни мнения, лайфстайл, сериали, филми, субтитри, технологии, наука, програмиране",
+    'MSpase.com - ви предоставя интересни новини и истории от България и света, свободни мнения, лайфстайл, сериали, филми, субтитри, технологии, наука, програмиране',
+  // mainImage:
+  //   "https://res.cloudinary.com/mayks/image/upload/v1653483806/blog-mayks/logo/mspase-logo.png",
   mainImage:
-    "https://res.cloudinary.com/mayks/image/upload/v1653483806/blog-mayks/logo/mspase-logo.png",
-  siteType: "website",
-};
+    'https://res.cloudinary.com/mayks/image/upload/v1654708617/blog-mayks/logo/logo_yibtdk.png',
+  siteType: 'website',
+}
