23151a07b6993ea42aadd0b392ef1b32f6d82a45
mayks1 <mayks1@gmail.com>
2022-06-15 12:33:26 +0300

    Add random image AdsCard

--- components/AdsCard.vue
+++ components/AdsCard.vue
@@ -1,7 +1,9 @@
 <template>
     <section class="aside-card">
         <h4 class="aside-card-title line-after">Вашата Реклама</h4>
-       <img src="https://res.cloudinary.com/mayks/image/upload/v1653666978/blog-mayks/banners/adds/ad-banner_yxukna.jpg" alt="Ads Picture">
+       <!-- <img src="https://res.cloudinary.com/mayks/image/upload/v1653666978/blog-mayks/banners/adds/ad-banner_yxukna.jpg" alt="Ads Picture"> -->
+       <img src="https://source.unsplash.com/random/400x600/?travel" loading="lazy" alt="Random People">
+
     </section>
 </template>
 
