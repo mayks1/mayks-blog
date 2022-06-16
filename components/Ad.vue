b0d92354e840d800028cff55b4fe5bf9100fe884
mayks1 <mayks1@gmail.com>
2022-06-16 17:40:44 +0300

    Refactor script Ad

--- components/Ad.vue
+++ components/Ad.vue
@@ -1,17 +1,5 @@
 <template>
   <div>
-    <!-- <template v-if="isDev">
-      [ADSENSE PLACEHOLDER]
-    </template>
-    <template v-else>
-     
-        <ins class="adsbygoogle"
-            style="display:block"
-            data-ad-client="ca-pub-8365005414967197"
-            data-ad-slot="9121674298"
-            data-ad-format="auto"
-            data-full-width-responsive="true"></ins>
-    </template> -->
     <ins class="adsbygoogle"
             style="display:block"
             data-ad-client="ca-pub-8365005414967197"
@@ -29,7 +17,7 @@ export default {
         script: [
           {
             hid: 'adsense',
-            src: "<https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8365005414967197>",
+            src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8365005414967197",
             defer: true,
             async: true,
             crossorigin: "anonymous"
